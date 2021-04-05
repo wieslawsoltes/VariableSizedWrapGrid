356a1be9b46b817069041de26463d00ff4d2aab1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-04-05 20:40:36 +0200

    Update TilePanelViewModel.cs

--- AvaloniaSample/ViewModels/TilePanelViewModel.cs
+++ AvaloniaSample/ViewModels/TilePanelViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using ReactiveUI;
 
 namespace AvaloniaSample.ViewModels
@@ -8,6 +9,8 @@ namespace AvaloniaSample.ViewModels
         private double _itemHeight;
         private double _itemWidth;
         private int _maximumRowsOrColumns;
+        private double _panelHeight;
+        private double _panelWidth;
         private ObservableCollection<TileViewModel> _tiles;
 
         public double ItemHeight
@@ -28,10 +31,37 @@ namespace AvaloniaSample.ViewModels
             set => this.RaiseAndSetIfChanged(ref _maximumRowsOrColumns, value);
         }
         
+        public double PanelHeight
+        {
+            get => _panelHeight;
+            set => this.RaiseAndSetIfChanged(ref _panelHeight, value);
+        }
+        
+        public double PanelWidth
+        {
+            get => _panelWidth;
+            set => this.RaiseAndSetIfChanged(ref _panelWidth, value);
+        }
+
         public ObservableCollection<TileViewModel> Tiles
         {
             get => _tiles;
             set => this.RaiseAndSetIfChanged(ref _tiles, value);
+        }
+
+        public TilePanelViewModel()
+        {
+            this.WhenAnyValue(x => x.PanelHeight)
+                .Subscribe(x =>
+                {
+                    // TODO: Update ItemHeight
+                });
+
+            this.WhenAnyValue(x => x.PanelWidth)
+                .Subscribe(x =>
+                {
+                    // TODO: Update ItemWidth
+                });
         }
     }
 }
