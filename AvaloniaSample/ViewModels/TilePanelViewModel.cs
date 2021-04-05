7d953b41132923a26bfb57529065f7b66e359a1b
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-04-05 20:11:40 +0200

    Create TilePanelViewModel.cs

---
+++ AvaloniaSample/ViewModels/TilePanelViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
+
+namespace AvaloniaSample.ViewModels
+{
+    public class TilePanelViewModel : ViewModelBase
+    {
+        private double _itemHeight;
+        private double _itemWidth;
+        private int _maximumRowsOrColumns;
+        private ObservableCollection<TileViewModel> _tiles;
+
+        public double ItemHeight
+        {
+            get => _itemHeight;
+            set => this.RaiseAndSetIfChanged(ref _itemHeight, value);
+        }
+        
+        public double ItemWidth
+        {
+            get => _itemWidth;
+            set => this.RaiseAndSetIfChanged(ref _itemWidth, value);
+        }
+        
+        public int MaximumRowsOrColumns
+        {
+            get => _maximumRowsOrColumns;
+            set => this.RaiseAndSetIfChanged(ref _maximumRowsOrColumns, value);
+        }
+        
+        public ObservableCollection<TileViewModel> Tiles
+        {
+            get => _tiles;
+            set => this.RaiseAndSetIfChanged(ref _tiles, value);
+        }
+    }
+}
