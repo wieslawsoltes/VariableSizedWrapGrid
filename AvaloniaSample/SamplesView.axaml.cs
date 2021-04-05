acd879126e40615514df4e8af648edff4a2fc811
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-04-05 20:11:18 +0200

    Added SamplesView

---
+++ AvaloniaSample/SamplesView.axaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace AvaloniaSample
+{
+    public class SamplesView : UserControl
+    {
+        public SamplesView()
+        {
+            InitializeComponent();
+
+            InitColors();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private void InitColors()
+        {
+            var colors = typeof(Colors)
+                .GetProperties()
+                .Select((c, i) => new
+                {
+                    Color = (Color) c.GetValue(null)!,
+                    Name = c.Name,
+                    Index = i,
+                    ColSpan = ColSpan(i),
+                    RowSpan = RowSpan(i)
+                });
+
+            DataContext = colors;
+
+            int RowSpan(int i)
+            {
+                if (i == 0)
+                    return 2;
+                if (i == 2)
+                    return 3;
+                if (i == 7)
+                    return 2;
+                if (i == 14)
+                    return 2;
+                return 1;
+            }
+
+            int ColSpan(int i)
+            {
+                if (i == 0)
+                    return 2;
+                if (i == 6)
+                    return 3;
+                if (i == 14)
+                    return 2;
+                return 1;
+            }
+        }
+    }
+}
